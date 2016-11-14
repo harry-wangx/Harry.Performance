44d3682c5e21698dac0fe9eacc6dbf85624778d4
Harry <harry_w@foxmail.com>
2016-11-14 17:07:42 +0800

    .

---
+++ src/Harry.Performance.Abstractions/ISpanCollector.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+//收集器接口
+//-----------------------------------------------------------------------
+#if !NET20 && !NET35
+using System.Threading.Tasks;
+#endif
+
+
+namespace Harry.Performance
+{
+    public interface ISpanCollector
+    {
+        void Collect(params Span[] spans);
+
+#if !NET20 && !NET35
+        Task CollectAsync(params Span[] spans);
+#endif
+
+
+    }
+}
---
+++ src/Harry.Performance/Collector/DebugCollector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+#if !NET20 && !NET35
+using System.Threading.Tasks;
+#endif
+
+namespace Harry.Performance.Collector
+{
+    public sealed class DebugCollector : ISpanCollector
+    {
+        private readonly TextWriter _writer;
+
+        public DebugCollector(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        /// <summary>
+        /// Creates a debug collector instance logging all data on the standard output.
+        /// </summary>
+        public DebugCollector() : this(System.Console.Out)
+        {
+        }
+
+        public void Collect(params Span[] spans)
+        {
+            foreach (var span in spans)
+                _writer.WriteLine(span.ToString());
+
+            _writer.Flush();
+        }
+
+
+#if !NET20 && !NET35
+
+        public
+#if ASYNC
+                        async
+#endif
+
+            Task CollectAsync(params Span[] spans)
+        {
+            foreach (var span in spans)
+                _writer.WriteLine(span.ToString());
+
+            _writer.Flush();
+            //throw new Exception();
+        }
+
+#endif
+    }
+}
